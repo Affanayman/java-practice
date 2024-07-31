b844eddf32cda846d2ab02ed5b2e8baa0eacfc71
Affanayman <164138787+Affanayman@users.noreply.github.com>
2024-07-31 14:28:22 +0530

    java practice programs

--- src/java8/Person.java
+++ src/java8/Person.java
@@ -1,8 +1,10 @@
+package java8;
+
 import java.util.Objects;
 
 public class Person {
-    private String name;
-    private int age;
+    private final String name;
+    private final int age;
 
     public Person(String name, int age) {
         this.name = name;
